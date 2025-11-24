69c1127ae6ac411404548141045fe2ff8b01670c
JaoVicy <filipimjoa@gmail.com>
2025-11-23 23:47:22 -0300

    refactor: change for size field

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ package main
 type pizza struct {
 	ID    string  `json:"id"`
 	Title string  `json:"title"`
-	Type  string  `json:"type"`
+	Size  string  `json:"size"`
 	Price float64 `json:"price"`
 }
 
 var pizzas = []pizza{
-	{ID: "1", Title: "Chocolate", Type: "John Coltrane", Price: 56.99},
-	{ID: "2", Title: "Barbecue", Type: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Peperoni", Type: "Sarah Vaughan", Price: 39.99},
+	{ID: "1", Title: "Chocolate", Size: "35", Price: 56.99},
+	{ID: "2", Title: "Barbecue", Size: "30", Price: 17.99},
+	{ID: "3", Title: "Peperoni", Size: "45", Price: 39.99},
 }
 
 func main() {
