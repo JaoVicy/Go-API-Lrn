ff59c1e3cacecfc22c7e61057b9d8c9f11c166f2
JaoVicy <filipimjoa@gmail.com>
2025-11-23 23:49:53 -0300

    feat: add func getPizzas

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 // album represents data about a record album.
 type pizza struct {
 	ID    string  `json:"id"`
@@ -12,6 +18,11 @@ var pizzas = []pizza{
 	{ID: "1", Title: "Chocolate", Size: "35", Price: 56.99},
 	{ID: "2", Title: "Barbecue", Size: "30", Price: 17.99},
 	{ID: "3", Title: "Peperoni", Size: "45", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getPizzas(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, pizzas)
 }
 
 func main() {
