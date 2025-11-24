cf94af5a4148c95f52a43d12648312e059beaa65
JaoVicy <filipimjoa@gmail.com>
2025-11-23 23:42:45 -0300

    feat: add list

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 // album represents data about a record album.
-type album struct {
-	ID     string  `json:"id"`
-	Title  string  `json:"title"`
-	Artist string  `json:"artist"`
-	Price  float64 `json:"price"`
+type pizza struct {
+	ID    string  `json:"id"`
+	Title string  `json:"title"`
+	Type  string  `json:"type"`
+	Price float64 `json:"price"`
+}
+
+var pizzas = []pizza{
+	{ID: "1", Title: "Blue Train", Type: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Type: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Type: "Sarah Vaughan", Price: 39.99},
 }
 
 func main() {
